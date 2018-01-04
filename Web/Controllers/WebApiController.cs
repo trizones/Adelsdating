645e12800f40a75831015c870172b79b0819a176
Martin Sarling <martinsarling97@gmail.se>
2018-01-04 16:01:08 +0100

    SCriptSaker

---
+++ Web/Controllers/WebApiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+
+            base.Dispose(disposing);
+        }
+        
+        public JsonResult HasRequest()
+        {
+            var user = db.Users.Single(x => x.UserName == User.Identity.Name);
+
+            bool hasRequest = false;
+
+            var Requests = db.Requests.Where(x => x.FromUser == user || x.ToUser == user && x.Accepted == false);
+            
+            if(Requests.Count() > 0)
+            {
+                hasRequest = true;
+            }
+
+            return new JsonResult { Data = hasRequest };
+        }
+    }
+}
--- Web/Scripts/scripts.js
+++ Web/Scripts/scripts.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(function() {
+    $.ajax({
+        type: "POST",
+        success: function (result) {
+            var url = "https://d30y9cdsu7xlg0.cloudfront.net/png/38220-200.png";
+            if (result.HasRequests) {
+                document.getElementById("hasRequests").src = url;
+            }
+        }
+    });
+});
