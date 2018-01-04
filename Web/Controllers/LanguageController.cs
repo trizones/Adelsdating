62c9ebdf158d8baebe904ddfec243c07dd1d9135
hogbergelliot <hogbergelliot@gmail.com>
2018-01-04 13:23:36 +0100

    fixxxx

---
+++ Web/Controllers/LanguageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(string LanguageAbbrevation)
+        {
+            if(LanguageAbbrevation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbbrevation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageAbbrevation);   
+            }
+
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = LanguageAbbrevation;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+    }
+}
